bdafa92f01b1c1f7639a640b70e3abad29b125a0
Alejandro199837 <alejito11palacios@gmail.com>
2025-08-26 08:25:44 -0500

    Descripción

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/proyecto_tres/', // Reemplaza 'proyecto_tres' con el nombre de tu repositorio
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
